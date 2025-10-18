c58ccb5f875cb8c99e7378a451b8be950a7a68bb
Ashbipbinu <abinu@thoughtlinetech.com>
2025-10-18 12:27:44 +0530

    unit test

--- src/models/model_test.py
+++ src/models/model_test.py
@@ -1,5 +1,6 @@
 import dagshub
 import mlflow
+import unittest
 import os
 
 from mlflow.tracking import MlflowClient
@@ -15,6 +16,40 @@ dagshub.auth.add_app_token(dags_hub_token)
 os.environ['MLFLOW_TRACKING_USERNAME'] = dags_hub_token
 os.environ['MLFLOW_TRACKING_PASSWORD'] = dags_hub_token
 
-model_name = "challenger"
+model_alias = "challenger"
+model_name = "Tuned_RandomForest"
 
 mlflow.set_tracking_uri('https://dagshub.com/Ashbipbinu/Water_Potability_MLFLOW.mlflow')
+
+class TestModelLoading(unittest.TestCase):
+
+    def test_model_challenger(self):
+        
+        client = MlflowClient()
+        version = client.get_model_version_by_alias(model_name, model_alias)
+
+        self.assertGreater(len(version), 0, "No challenger models found")
+    
+    def test_model_loading(self):
+        client = MlflowClient()
+        version = client.get_model_version_by_alias(model_name, model_alias)
+
+        if not version:
+            self.fail("No model found with the alias challenger")
+        
+        latest_version = version[0].version
+        run_id = version[0].run_id
+
+        logged_model = f"runs:/{run_id}/{model_name}"
+
+        try:
+
+            loaded_model = mlflow.pyfunc.load_model(logged_model)
+        except Exception as e:
+            self.fail(f"Failed to load model: {e}")
+        
+        self.assertIsNotNone(loaded_model, "The loaded model is none")
+        print(f"Model succesfully loaded from {logged_model}")
+
+if __name__ == "__main__":
+    unittest.main()
